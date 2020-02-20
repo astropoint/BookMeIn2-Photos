0f741ad774bf68108a5bfb23786079cb99c8d1d8
James Rooney <james_rooney@hotmail.com>
2020-02-20 08:47:24 +0000

    Actually storing photcount when increased this time

--- www/js/index.js
+++ www/js/index.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 	}
 });
 
+var debugging = false;
 var photocount;
 
 var spinner = '<svg class="svg-inline--fa fa-spinner fa-w-16 fa-spin" aria-hidden="true" data-prefix="fas" data-icon="spinner" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"></path></svg>';
@@ -85,6 +86,7 @@ $(document).on('click', '#save', function(e){
 	localStorage.setItem("contact_"+photocount+"_notes", notes);
 	
 	photocount++;
+	localStorage.setItem("photocount", photocount);
 	$('#notes').val('');
 	$('#photoid').attr('src', '')
 });
@@ -107,21 +109,25 @@ function viewPhotos(){
 	html += "</tbody></table>";
 	$('#allcontacts').html(html);
 	
-	var content = "";
-	var localStorageSpace = function(){
-        var allStrings = '';
-        for(var key in window.localStorage){
-            if(window.localStorage.hasOwnProperty(key)){
-                allStrings += window.localStorage[key];
-            }
-        }
-        return allStrings ? 3 + ((allStrings.length*16)/(8*1024)) + ' KB' : 'Empty (0 KB)';
-    };
-  content += "<pre>Total size: "+localStorageSpace()+"</pre>";
-	for (var i = 0; i < localStorage.length; i++){
-		content += "<pre>"+localStorage.key(i)+": "+localStorage.getItem(localStorage.key(i))+"</pre>";
+	if(debugging){
+		var content = "";
+		var localStorageSpace = function(){
+					var allStrings = '';
+					for(var key in window.localStorage){
+							if(window.localStorage.hasOwnProperty(key)){
+									allStrings += window.localStorage[key];
+							}
+					}
+					return allStrings ? 3 + ((allStrings.length*16)/(8*1024)) + ' KB' : 'Empty (0 KB)';
+			};
+		content += "<pre>Total size: "+localStorageSpace()+"</pre>";
+		for (var i = 0; i < localStorage.length; i++){
+			content += "<pre>"+localStorage.key(i)+": "+localStorage.getItem(localStorage.key(i))+"</pre>";
+		}
+		$('#allstorage').html(content);
+	}else{
+		$('#pendingblock').hide();
 	}
-	$('#allstorage').html(content);
 }
 
 function dateWithoutSeconds(date){
