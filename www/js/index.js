2f535163785e9d8fbb7905a2b60b5dc87d778e69
James Rooney <james_rooney@hotmail.com>
2020-02-20 08:34:18 +0000

    Fixed issue with number of photos variables not being persistant

--- www/js/index.js
+++ www/js/index.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 	//This will need to be removed when it's uploaded to phone...
 	document.addEventListener('deviceready', onDeviceReady,false);
 	
-	
+	photocount = localStorage.getItem("photocount");
+	if(photocount===null){
+		photocount = 0;
+		localStorage.setItem("photocount", 0);
+	}
 });
 
-var photocount = 0;
-var agendasort = 'desc';
-var loggedIn = false;
+var photocount;
 
 var spinner = '<svg class="svg-inline--fa fa-spinner fa-w-16 fa-spin" aria-hidden="true" data-prefix="fas" data-icon="spinner" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"></path></svg>';
 
