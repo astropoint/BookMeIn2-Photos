5a0cda2f2117873c04f0b7c4a11ffdeba64f4ee3
James Rooney <james_rooney@hotmail.com>
2020-03-10 09:45:40 +0000

    Photos can be uploaded to a special page on the bookmein2 website

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
 	//This will need to be removed when it's uploaded to phone...
 	document.addEventListener('deviceready', onDeviceReady,false);
+	checkInternet();
+	
+	//check the status of the internet every 10 seconds
+	setInterval(function(){
+		checkInternet();
+	}
 	
 	photocount = localStorage.getItem("photocount");
 	if(photocount===null){
 		photocount = 0;
 		localStorage.setItem("photocount", 0);
 	}
+	
+	batchnumber = localStorage.getItem("batchnumber");
+	if(batchnumber===null){
+		batchnumber = 0;
+		localStorage.setItem("batchnumber", 0);
+	}
 });
 
 var debugging = false;
 var photocount;
+var isInternet = false;
+var apiURL = "https://photos.jdrcomputers.co.uk/api/api.php";
 
 var spinner = '<svg class="svg-inline--fa fa-spinner fa-w-16 fa-spin" aria-hidden="true" data-prefix="fas" data-icon="spinner" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"></path></svg>';
 
@@ -52,6 +66,29 @@ $(document).on('click', '.goback', function(e){
 	window.history.back();
 });
 
+function checkInternet(){
+	var data = "action=checkconnection"; 
+	var success = false;
+	$.ajax({
+			url: apiURL,
+			data: data,
+			dataType: "json",
+			type: 'post', 
+			error: function(){
+					// will fire when timeout is reached
+					isInternet = false;
+			},
+			success: function(response){
+				if(response.success){
+					isInternet = true;
+				}else{
+					isInternet = false;
+				}
+			},
+			timeout: 3000 // sets timeout to 3 seconds
+	});
+}
+
 $(document).on('click', '#getphotobutton', function(e){
 
 	
@@ -84,6 +121,7 @@ $(document).on('click', '#save', function(e){
 	
 	localStorage.setItem("contact_"+photocount+"_photo", photo);
 	localStorage.setItem("contact_"+photocount+"_notes", notes);
+	localStorage.setItem("contact_"+photocount+"_uploaded", "0");
 	
 	photocount++;
 	localStorage.setItem("photocount", photocount);
@@ -188,6 +226,118 @@ function viewSinglePhoto(){
 	$('#viewsinglephoto').attr('src', localStorage.getItem("contact_"+thisid+"_photo"));
 }
 
+function createBatch(){
+	var data = "action=createbatch";
+	
+	if(isInternet){
+		//check to see if batch has already been created
+		batchset = localStorage.getItem("batch_"+batchnumber+"_set");
+		if(batchset===null || batchset!='1'){
+			//not been set, set it
+			$.ajax({
+				url: apiURL,
+				data: data,
+				dataType: "json",
+				type: 'post'
+			}).done(function(response){
+				if(response.success){
+					
+					localStorage.setItem("batch_"+batchnumber+"_set", "1");
+					localStorage.setItem("batch_"+batchnumber+"_batchid", response.batchid);
+					localStorage.setItem("batch_"+batchnumber+"_url", response.url);
+					localStorage.setItem("batch_"+batchnumber+"_password", response.password);
+					//once set, try to upload photos
+					tryToUploadPhotos();
+					
+				}else{
+					$('#feedback').html(response.message);
+				}
+			});
+		}else{
+			tryToUploadPhotos();
+		}
+		
+		
+	}
+}
+
+function tryToUploadPhotos(){
+	if(isInternet){
+		for(var i=0;i<photocount;i++){
+			if(localStorage.getItem("contact_"+i+"_uploaded")=='0'){
+				uploadPhoto(i);
+				return;
+			}
+		}
+		//if we are here, then everything should have been uploaded
+		photocount = 0;
+		batchnumber++;
+		//redirect to view batches page?
+		
+	}else{
+		//check the internet connection (how are we here if it isn't on already?)
+		checkInternet();
+		//wait 5 seconds then try again
+		setTimeout(tryToUploadPhotos(), 5000);
+	}
+	
+}
+
+function uploadPhoto(photoid){
+	if(isInternet){
+		
+		var batchurl = localStorage.getItem("batch_"+batchnumber+"_url");
+		var password = localStorage.getItem("batch_"+batchnumber+"_password");
+		var photobase64 = localStorage.getItem("batch_"+batchnumber+"_photo");
+		var notes = localStorage.getItem("batch_"+batchnumber+"_notes");
+		
+		var data = "action=uploadphoto&url="+batchurl+"&password="+password+"&photo="+photo+"&notes=notes";
+		
+		$.ajax({
+			url: apiURL,
+			data: data,
+			dataType: "json",
+			type: 'post'
+		}).done(function(response){
+			if(response.success){
+				
+				localStorage.setItem("batch_"+batchnumber+"_uploaded", "1");
+				//once uploaded, try to upload the next photo
+				tryToUploadPhotos();
+				
+			}else{
+				$('#feedback').html(response.message);
+			}
+			tryToUploadPhotos();
+		}).fail(function(response){
+			tryToUploadPhotos();
+		});
+		
+	}else{
+		//check the internet connection (how are we here if it isn't on already?)
+		checkInternet();
+		//wait 5 seconds then try again
+		setTimeout(tryToUploadPhotos(), 5000);
+	}
+}
+
+function viewBatches(){
+	var html = "";
+	if(batchnumber>0){
+		for(var i = 0; i<batchnumber;i++){
+			if(localStorage.getItem("batch_"+i+"_uploaded") == "1"){
+				html += "<tr>";
+				html += "<td><a href='https://photos.jdrcomputers.co.uk/"+localStorage.getItem("batch_"+i+"_url")+"/'>https://photos.jdrcomputers.co.uk/"+localStorage.getItem("batch_"+i+"_url")+"/</a></td>";
+				html += "<td>"+localStorage.getItem("batch_"+i+"_password")+"</td>";
+				html += "</tr>";
+			}
+		}
+	}else{
+		html = "<tr><td colspan='2'>No batches have been uploaded to the website</td></tr>";
+	}
+	$('#batchbody').html(html);
+}
+
 //Page change listener - calls functions to make this readable. NB due to the way the "pages" are loaded we cannot put this inside the document ready function.
 //Sham - this and the below are there for expandability, can be used for selective synch so only page relevant data is refreshed.
 $(document).on( "pagecontainerchange", function( event, ui ) {
@@ -203,6 +353,9 @@ $(document).on( "pagecontainerchange", function( event, ui ) {
 			break;
 		case "viewlist":
 			viewPhotos();
+			break;
+		case "batchlist":
+			viewBatches();
 			break;
 		default:
 			console.log("NO PAGE INIT FUNCTION: "+ui.toPage.prop("id"));
